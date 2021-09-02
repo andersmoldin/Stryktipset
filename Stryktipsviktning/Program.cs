0fe4fe6f20a23bd296e1ff88a1ec4feb32257a88
Anders Moldin <andersmoldin@Skierfe.local>
2021-09-02 08:12:03 +0200

    Refined project

--- Stryktipsviktning/Program.cs
+++ Stryktipsviktning/Program.cs
@@ -1,34 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Ardalis.SmartEnum;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
 using Weighted_Randomizer;
 
 namespace Stryktipsviktning
 {
     class Program
     {
+        static string html;
+
         static void Main(string[] args)
         {
-            var kupong = new Dictionary<int, DynamicWeightedRandomizer<string>>();
-            var temp = 0;
+            Console.Clear();
+
+            var oddsfavoritskap = HämtaOddsfavoritskapOchSpelvärde(Värde.Oddsfavoritskap);
+            SkrivUtKupong(oddsfavoritskap, Värde.Oddsfavoritskap);
+
+            Console.WriteLine();
+
+            var spelvärde = HämtaOddsfavoritskapOchSpelvärde(Värde.Spelvärde);
+            SkrivUtKupong(spelvärde, Värde.Spelvärde);
+        }
+
+        private static Dictionary<int, DynamicWeightedRandomizer<string>> HämtaOddsfavoritskapOchSpelvärde(Värde värde)
+        {
+            var viktning = new Dictionary<int, DynamicWeightedRandomizer<string>>();
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(GetHtml("https://stryket.se/stryktipset"));
+
+            var rows = htmlDoc.DocumentNode.SelectNodes("//table//tbody//tr");
+
+            for (int i = 0; i < rows.Count; i++)
+            {
+                var row = rows[i].ChildNodes.Where(r => r.Name == "td").ToList();
+
+                viktning.Add(i, new DynamicWeightedRandomizer<string>());
+
+                for (int j = 0; j < 3; j++)
+                {
+                    viktning[i].Add(Tecken.FromValue(j + 1).Name, int.Parse(row[j + (int)värde].InnerText.Replace(".", "")));
+                }
+            }
+
+            return viktning;
+        }
+
+        private static string GetHtml(string url)
+        {
+            if (string.IsNullOrEmpty(html))
+            {
+                var driver = new SafariDriver();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                driver.Navigate().GoToUrl(url);
+                var table = driver.FindElement(By.XPath("/html/body/app-root/body/div/app-draw-value/table"));
+                html = driver.PageSource;
+            }
+
+            return html;
+        }
+
+        private static void TaEmotInputOchSkapaEnViktadKupong()
+        {
+            var viktning = new Dictionary<int, DynamicWeightedRandomizer<string>>();
 
             for (int i = 0; i < 13; i++)
             {
-                kupong.Add(i, new DynamicWeightedRandomizer<string>());
+                viktning.Add(i, new DynamicWeightedRandomizer<string>());
 
                 for (int j = 0; j < 3; j++)
                 {
                     Console.WriteLine($"För match {i + 1}, andelsvikta {Tecken.FromValue(j + 1)}:");
-                    temp = Convert.ToInt32(Console.ReadLine());
+                    int temp = Convert.ToInt32(Console.ReadLine());
                     Console.Clear();
 
-                    kupong[i].Add(Tecken.FromValue(j + 1).Name, temp);
+                    viktning[i].Add(Tecken.FromValue(j + 1).Name, temp);
                 }
             }
+        }
 
-            Console.Clear();
-            Console.WriteLine("Slumpad rad:");
-            foreach (var rad in kupong)
+        private static void SkrivUtKupong(Dictionary<int, DynamicWeightedRandomizer<string>> viktning, Värde värde)
+        {
+            Console.WriteLine($"Slumpad {värde.ToString().ToLower()}srad:");
+            foreach (var rad in viktning)
             {
                 var tecken = rad.Value.NextWithReplacement();
 
@@ -56,5 +114,10 @@ namespace Stryktipsviktning
             private Tecken(int value, string displayName) : base(displayName, value) { }
         }
 
+        public enum Värde
+        {
+            Oddsfavoritskap = 8,
+            Spelvärde = 11
+        }
     }
 }
