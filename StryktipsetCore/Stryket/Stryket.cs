1d9d2bafaaf77e2162922040677fe736520ce2b9
Anders Moldin <anders.moldin@lakemedelsverket.se>
2021-09-03 17:39:35 +0200

    Added API call

---
+++ StryktipsetCore/Stryket/Stryket.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace StryktipsetCore.Stryket
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Odds
+    {
+        [JsonProperty("home")]
+        public decimal Home { get; set; }
+
+        [JsonProperty("draw")]
+        public decimal Draw { get; set; }
+
+        [JsonProperty("away")]
+        public decimal Away { get; set; }
+
+        public decimal Total => Home + Draw + Away;
+
+        public int HomePercent => Convert.ToInt32(Total / Home * 100);
+    }
+
+    public class Distribution
+    {
+        [JsonProperty("home")]
+        public string Home { get; set; }
+
+        [JsonProperty("draw")]
+        public string Draw { get; set; }
+
+        [JsonProperty("away")]
+        public string Away { get; set; }
+    }
+
+    public class NewspaperAdvice
+    {
+        [JsonProperty("home")]
+        public string Home { get; set; }
+
+        [JsonProperty("draw")]
+        public string Draw { get; set; }
+
+        [JsonProperty("away")]
+        public string Away { get; set; }
+    }
+
+    public class Season
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Country
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class League
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("season")]
+        public Season Season { get; set; }
+
+        [JsonProperty("country")]
+        public Country Country { get; set; }
+    }
+
+    public class Participant
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Event
+    {
+        [JsonProperty("eventNumber")]
+        public int EventNumber { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("cancelled")]
+        public bool Cancelled { get; set; }
+
+        [JsonProperty("extraInfo")]
+        public object ExtraInfo { get; set; }
+
+        [JsonProperty("eventTypeDescription")]
+        public string EventTypeDescription { get; set; }
+
+        [JsonProperty("participantType")]
+        public string ParticipantType { get; set; }
+
+        [JsonProperty("outcomes")]
+        public object Outcomes { get; set; }
+
+        [JsonProperty("odds")]
+        public Odds Odds { get; set; }
+
+        [JsonProperty("distribution")]
+        public Distribution Distribution { get; set; }
+
+        [JsonProperty("newspaperAdvice")]
+        public NewspaperAdvice NewspaperAdvice { get; set; }
+
+        [JsonProperty("league")]
+        public League League { get; set; }
+
+        [JsonProperty("participants")]
+        public List<Participant> Participants { get; set; }
+
+        [JsonProperty("sportEventId")]
+        public int SportEventId { get; set; }
+
+        [JsonProperty("sportEventStart")]
+        public DateTime SportEventStart { get; set; }
+    }
+
+    public class Week
+    {
+        [JsonProperty("drawComment")]
+        public string DrawComment { get; set; }
+
+        [JsonProperty("extraInfo")]
+        public object ExtraInfo { get; set; }
+
+        [JsonProperty("fund")]
+        public object Fund { get; set; }
+
+        [JsonProperty("events")]
+        public List<Event> Events { get; set; }
+
+        [JsonProperty("productName")]
+        public string ProductName { get; set; }
+
+        [JsonProperty("productId")]
+        public int ProductId { get; set; }
+
+        [JsonProperty("drawNumber")]
+        public int DrawNumber { get; set; }
+
+        [JsonProperty("openTime")]
+        public DateTime OpenTime { get; set; }
+
+        [JsonProperty("closeTime")]
+        public DateTime CloseTime { get; set; }
+
+        [JsonProperty("turnover")]
+        public string Turnover { get; set; }
+
+        [JsonProperty("sport")]
+        public string Sport { get; set; }
+
+        [JsonProperty("sportId")]
+        public int SportId { get; set; }
+
+        [JsonProperty("checksum")]
+        public string Checksum { get; set; }
+    }
+
+
+}
--- Stryktipsviktning/Program.cs
+++ Stryktipsviktning/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Ardalis.SmartEnum;
 using HtmlAgilityPack;
+using Newtonsoft.Json;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Safari;
+using StryktipsetCore.Stryket;
 using Weighted_Randomizer;
 
 namespace Stryktipsviktning
@@ -12,18 +21,91 @@ namespace Stryktipsviktning
     class Program
     {
         static string html;
+        private static readonly HttpClient client = new HttpClient();
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.Clear();
 
-            var oddsfavoritskap = HämtaOddsfavoritskapOchSpelvärde(Värde.Oddsfavoritskap);
+            var oddsfavoritskap = await MasseraApiOchRäknaUtOddsfavoritskapOchSpelvärde(Värde.Oddsfavoritskap);
             SkrivUtKupong(oddsfavoritskap, Värde.Oddsfavoritskap);
-
+            
             Console.WriteLine();
 
-            var spelvärde = HämtaOddsfavoritskapOchSpelvärde(Värde.Spelvärde);
+            var spelvärde = await MasseraApiOchRäknaUtOddsfavoritskapOchSpelvärde(Värde.Spelvärde);
             SkrivUtKupong(spelvärde, Värde.Spelvärde);
+
+            //var oddsfavoritskap = HämtaOddsfavoritskapOchSpelvärde(Värde.Oddsfavoritskap);
+            //SkrivUtKupong(oddsfavoritskap, Värde.Oddsfavoritskap);
+
+            //Console.WriteLine();
+
+            //var spelvärde = HämtaOddsfavoritskapOchSpelvärde(Värde.Spelvärde);
+            //SkrivUtKupong(spelvärde, Värde.Spelvärde);
+        }
+
+        private static async Task<Dictionary<int, DynamicWeightedRandomizer<string>>> MasseraApiOchRäknaUtOddsfavoritskapOchSpelvärde(Värde värde)
+        {
+            var viktning = new Dictionary<int, DynamicWeightedRandomizer<string>>();
+            var response = await HämtaFrånStryketApi();
+            var currentWeek = response.OrderByDescending(r => r.CloseTime).First().Events;
+
+            for (int i = 0; i < currentWeek.Count; i++)
+            {
+                viktning.Add(i, new DynamicWeightedRandomizer<string>());
+
+                switch (värde)
+                {
+                    case Värde.Oddsfavoritskap:
+                        if (currentWeek[i].Odds != null)
+                        {
+                            viktning[i].Add(Tecken.Ett.Name, decimal.ToInt32(1 / currentWeek[i].Odds.Home * 100));
+                            viktning[i].Add(Tecken.Kryss.Name, decimal.ToInt32(1 / currentWeek[i].Odds.Draw * 100));
+                            viktning[i].Add(Tecken.Två.Name, decimal.ToInt32(1 / currentWeek[i].Odds.Away * 100));
+                        }
+                        else
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                viktning[i].Add(Tecken.FromValue(j + 1).Name, 1);
+                            }
+                        }
+                        break;
+
+                    case Värde.Spelvärde:
+                        if (currentWeek[i].Odds != null && currentWeek[i].Distribution != null)
+                        {
+                            viktning[i].Add(Tecken.Ett.Name, decimal.ToInt32((1 / currentWeek[i].Odds.Home * 100) / decimal.Parse(currentWeek[i].Distribution.Home) * 100));
+                            viktning[i].Add(Tecken.Kryss.Name, decimal.ToInt32((1 / currentWeek[i].Odds.Draw * 100) / decimal.Parse(currentWeek[i].Distribution.Draw) * 100));
+                            viktning[i].Add(Tecken.Två.Name, decimal.ToInt32((1 / currentWeek[i].Odds.Away * 100) / decimal.Parse(currentWeek[i].Distribution.Away) * 100));
+                        }
+                        else
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                viktning[i].Add(Tecken.FromValue(j + 1).Name, 1);
+                            }
+                        }
+                        break;
+                }
+
+            }
+
+            return viktning;
+        }
+
+        private static async Task<Week[]> HämtaFrånStryketApi()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://stryket.se/api/draws/stryktipset");
+
+            //var stringTask = File.ReadAllText("tempStringTask.txt");
+
+            return JsonConvert.DeserializeObject<Week[]>(await stringTask, new JsonSerializerSettings { Culture = new CultureInfo("sv-SE") });
         }
 
         private static Dictionary<int, DynamicWeightedRandomizer<string>> HämtaOddsfavoritskapOchSpelvärde(Värde värde)
@@ -54,11 +136,25 @@ namespace Stryktipsviktning
         {
             if (string.IsNullOrEmpty(html))
             {
-                var driver = new SafariDriver();
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.Navigate().GoToUrl(url);
-                var table = driver.FindElement(By.XPath("/html/body/app-root/body/div/app-draw-value/table"));
-                html = driver.PageSource;
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    var driver = new SafariDriver();
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    driver.Navigate().GoToUrl(url);
+                    var table = driver.FindElement(By.XPath("/html/body/app-root/body/div/app-draw-value/table"));
+                    html = driver.PageSource;
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    var options = new ChromeOptions
+                    {
+                    };
+                    var driver = new ChromeDriver();
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    driver.Navigate().GoToUrl(url);
+                    var table = driver.FindElement(By.XPath("/html/body/app-root/body/div/app-draw-value/table"));
+                    html = driver.PageSource;
+                }
             }
 
             return html;
@@ -90,16 +186,18 @@ namespace Stryktipsviktning
             {
                 var tecken = rad.Value.NextWithReplacement();
 
+                var hej = rad.Value.Skip(0).Take(1);
+
                 switch (tecken)
                 {
                     case "1":
-                        Console.WriteLine("1");
+                        Console.WriteLine($"1   ({rad.Value["1"]} {rad.Value["X"]} {rad.Value["2"]})");
                         break;
                     case "X":
-                        Console.WriteLine(" X");
+                        Console.WriteLine($" X  ({rad.Value["1"]} {rad.Value["X"]} {rad.Value["2"]})");
                         break;
                     case "2":
-                        Console.WriteLine("  2");
+                        Console.WriteLine($"  2 ({rad.Value["1"]} {rad.Value["X"]} {rad.Value["2"]})");
                         break;
                 }
             }
